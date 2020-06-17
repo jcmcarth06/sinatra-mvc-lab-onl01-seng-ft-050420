496dce742cde3d187507c20a12071898c6dbdf96
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-17 22:56:38 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,26 +1,26 @@
 class PigLatinizer
 
-    def piglatinize(string)
-        split_str = string.split(' ')
-        pig_str = split_str.map {|word| piglatinize_word(word)}
-        pig_str.join(' ')
+  def initialize
+  end
 
-    end
-
-    def piglatinize_word(word)
-        first_letter = word[0].downcase
-        if ["a", "e", "i", "o", "u"].include?(first_letter)
-            "#{word}way"
-        else
-            consonants = []
-            consonants << word[0]
-              if !["a", "e", "i", "o", "u"].include?(word[1]
-                consonants << word[1]
-                if !["a", "e", "i", "o", "u"].include?(word[2])
-                consonants << word[2]
+  def piglatinize(text)
+      combine = []
+      text_array = text.split(" ")
+      text_array.each do |word|
+          word_array = word.split("")
+          if ["A", "E", "I", "O", "U"].include?(word_array.first)
+              "#{word}way"
+              combine << word_array.join()
+          else
+              consonants = []
+              consonants << word[0]
+                if !["a", "e", "i", "o", "u"].include?(word[1])
+                  consonants << word[1]
+                if !["a", "e", "i", "o", "u"].include?(word[1])
+                  consonants << word[2]
                 end
-            end
-            "#{word.slice(consonants.length , word.length)}#{consonants.join}ay"
-        end
-    end
+          end
+      end
+      combine.join(" ")
+  end
 end
