323356990cca686a1db36941d4e74f604daf900c
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-17 22:30:16 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,18 +1,21 @@
 class Piglatinizer
-def piglatinize(text)
-    combine = []
-    text_array = text.downcase.split(" ")
-    text_array.each do |word|
-        word_array = word.split("")
-        if ["A", "E", "I", "O", "U"].include?(word_array.first)
-            word_array.push("w", "a", "y")
-            combine << word_array.join()
-        else
-            first = word_array.shift()
-            word_array.push(first, "a", "y")
-            combine << word_array.join()
-        end
-    end
-    combine.join(" ")
-end
+
+  def initialize
+  end
+  def piglatinize(text)
+      combine = []
+      text_array = text.downcase.split(" ")
+      text_array.each do |word|
+          word_array = word.split("")
+          if ["A", "E", "I", "O", "U"].include?(word_array.first)
+              word_array.push("w", "a", "y")
+              combine << word_array.join()
+          else
+              first = word_array.shift()
+              word_array.push(first, "a", "y")
+              combine << word_array.join()
+          end
+      end
+      combine.join(" ")
+  end
 end
